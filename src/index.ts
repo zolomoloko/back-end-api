96b0b718632c6130aacfa3147437b8ef31fcdbd5
25LP3742 25LP3742 <Zoljargal.g247@gmail.com>
2025-06-16 13:25:24 +0800

    hmm

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import fs from "fs-extra";
+import express, { Request, Response } from "express";
+import { User } from "./types";
+const app = express(); //ene n shuud neg server asaaj ugdug
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send({ name: "hello world" , age: "1"});
+});
+app.post("/user", (req: Request, res:Response) => { // url n uur baih ystoi
+    const {name, age} = req.body;
+    res.json({ message: `User ${name} is ${age} yers old.`})
+})
+app.put("/updateUser", (req: Request, res:Response) => {
+    const { name, age }=req.body
+    res.send(`updated user ${name} ${age}`);
+})
+
+app.delete("/deleteUser", (req: Request, res:Response) => {
+  const { userId } = req.body;
+  res.send(`deleted user id ${userId}`);
+});
+
+app.post("/createUser", (req: Request, res: Response) => {
+  const { name, age, userName, userEmail, phoneNumber, password }: User =
+    req.body;
+
+  fs.writeFileSync(
+    "./user.json",
+    JSON.stringify([
+      {
+        name,
+        age,
+        userName,
+        userEmail,
+        phoneNumber,
+        password,
+      },
+    ])
+  );
+
+  res.send("Successfully created User");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
+  res.json(JSON.parse(users));
+});
+
+app.listen(3000, () => {
+  // hed deer asaah portoo bichij ugnu
+  console.log(`Example app listening on port http://localhost:3000`);
+});
